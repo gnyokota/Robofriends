b00368692a8f54c34062155abe2bc2d6980524f6
Giovana Yokota <gnyokota@gmail.com>
2020-11-19 15:08:36 +0100

    changed favicon

--- src/App.js
+++ src/App.js
@@ -15,20 +15,18 @@ function App(){
         setUser({...user, searchField : event.target.value}) 
     }
 
-        const filteredRobots = user.robots.filter(robot=>{
-             return robot.name.toLowerCase().includes(user.searchField.toLowerCase());
-        });
-
-        return(
-            <div>
-
-                <h1 className='title'>Robofriends</h1>
-                {/* since this is a class, I need to add this */}
-                <SearchBox searchChange={onSearchChange} />
-                <CardList robots={filteredRobots} />
-                <p className="attribution">Coded by <a href="https://www.linkedin.com/in/gyokota/">Giovana Yokota</a></p>
-            </div>
-        )
+    const filteredRobots = user.robots.filter(robot=>{
+         return robot.name.toLowerCase().includes(user.searchField.toLowerCase());
+    });
+    return(
+        <div>
+            <h1 className='title'>Robofriends</h1>
+            {/* since this is a class, I need to add this */}
+            <SearchBox searchChange={onSearchChange} />
+            <CardList robots={filteredRobots} />
+            <p className="attribution">Coded by <a href="https://www.linkedin.com/in/gyokota/">Giovana Yokota</a></p>
+        </div>
+    )
 
 }
 
